3c3d8ade4733c9e4f527ec87b589403b7f5c2476
pyguru123 <pyguru123@gmail.com>
2023-01-31 22:15:44 +0530

    added visitor pattern

--- Behavioral Pattern/visitor.py
+++ Behavioral Pattern/visitor.py
@@ -3,7 +3,7 @@ class House:
 
 	def accept(self, visitor):
 		"""Interface to accept a visitor"""
-		# Triggers the visiting operation
+		visitor.visit(self) # Triggers the visiting operation
 
 
 	def work_on_havc(self, hvac_specialist):
@@ -15,3 +15,27 @@ class House:
 	def __str__(self):
 		"""Simply return the class name when house is being printed"""
 		return self.__class__.__name__
+
+class Visitor:
+	"""Abstract Visitor"""
+	def __str__(self):
+		"""Simply return the class name when Visitor object is printed"""
+		return self.__class__.__name__
+
+class HvacSpecialist(Visitor):
+	"""Concrete Visitor : hvac specialist"""
+	def visit(self, house):
+		house.work_on_havc(self)
+
+class Electrician(Visitor):
+	"""Concrete Visitor: electrician"""
+	def visit(self, house):
+		house.work_on_electricity(self)
+
+hvac = HvacSpecialist()
+electrician = Electrician()
+house = House()
+
+house.accept(hvac)
+house.accept(electrician)
+print(house)
